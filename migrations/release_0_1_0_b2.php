19e77f11ce89f862585bb50798213e3dbb443f8f
andreaskou <akourtidis@gmail.com>
2017-12-15 20:34:13 +0100

    minor fix on depends_on

--- migrations/release_0_1_0_b2.php
+++ migrations/release_0_1_0_b2.php
@@ -1,18 +1,24 @@
 <?php
-/** 
-* 
-* @package Prime Post Revision
-* @copyright (c) 2016 Bruninoit
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2 
-* 
-*/ 
+/**
+ *
+ * @package Prime Post Revision
+ * @copyright (c) 2016 Bruninoit
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+*/
+
 namespace bruninoit\ppr\migrations;
+
 class release_0_1_0_b2 extends \phpbb\db\migration\migration
 {
 	static public function depends_on()
 	{
-		return array('\phpbb\db\migration\data\v310\gold');
+		return array(
+			'\phpbb\db\migration\data\v310\gold',
+			'\bruinoit\ppr\migration\release_0_1_0',
+		);
 	}
+	
 	public function update_schema()
 	{
 return array(
